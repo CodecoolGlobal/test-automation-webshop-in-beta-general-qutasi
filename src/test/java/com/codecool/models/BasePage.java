6ecd36232d5032f670d19a85c03bf7573d05c848
faboknorbert <fabok.norbert94@gmail.com>
2024-12-10 13:13:54 +0100

    Add base page model

---
+++ src/test/java/com/codecool/models/BasePage.java
@@ -0,0 +1,26 @@
+package com.codecool.models;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+    protected final WebDriver driver;
+    protected final WebDriverWait wait;
+    protected final Actions actions;
+
+    public BasePage(WebDriver driver, WebDriverWait wait, Actions actions) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+        this.wait = wait;
+        this.actions = actions;
+    }
+
+    public void goToShoppingCartPage(){
+        WebElement shoppingCart = driver.findElement(By.xpath("//a[@data-test='shopping-cart-link']"));
+        shoppingCart.click();
+    }
+}
